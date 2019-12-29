3ce2604910e18696df3daba9624f70d8c637d2af
Tom Fahner <tom.fahner@actiflow.com>
2019-12-29 16:27:04 +0100

    First 4 exercises

---
+++ chapter_01/ex3.py
@@ -0,0 +1,23 @@
+print("I will now count my chickens:")
+# Shows the number of hens and roosters
+print("Hens", 25.0 + 30 / 6) # 25 + 5
+print("Roosters", 100.0 - 25 * 3 % 4) # 100 - 75 % 4
+
+print("Now I will count the eggs:")
+
+print(3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+#The next line prints the result of the logical test
+print(3 + 2.0 < 5 - 7.0)
+
+print("What is 3 + 2?", 3 + 2.0)
+print("What is 5 - 7?", 5 - 7.0)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
