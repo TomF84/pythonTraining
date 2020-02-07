2c9920728d7ea1b8f6b2eea631b4954122bf3a14
Tom Fahner <tom.fahner@actiflow.com>
2020-02-07 16:34:06 +0100

    Done some exercises on ex17

--- chapter_01/ex17.py
+++ chapter_01/ex17.py
@@ -6,19 +6,20 @@ script, from_file, to_file = argv
 print(f"Copying from {from_file} to {to_file}")
 
 # we could do these on one line, how? indata = open(from_file, 'r') or open(from_file).read() ?
-in_file = open(from_file)
-indata = in_file.read()
 
-print(f"The input file is {len(indata)} bytes long")
+#in_file = open(from_file)
+indata = open(from_file).read() #indata = in_file.read()
+
+#print(f"The input file is {len(indata)} bytes long")
 
 print(f"Does the ouput file exist? {exists(to_file)}")
-print("Ready, hit RETURN to continue, CTRL-C to abort.")
-input()
+#print("Ready, hit RETURN to continue, CTRL-C to abort.")
+#input()
 
 out_file = open(to_file, 'w')
 out_file.write(indata)
 
-print("Allright, all done")
+#print("Allright, all done")
 
-out_file.close()
-in_file.close()
+#out_file.close()
+#in_file.close()
