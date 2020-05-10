85357450d459e6c5c1f7a6a2cf375648cc4dba6e
Tom Fahner <tom.fahner@actiflow.com>
2020-05-10 22:04:02 +0200

    Added new information in Index.txt and made basic game in ex36.py

--- chapter_01/ex36.py
+++ chapter_01/ex36.py
@@ -1,78 +1,131 @@
 from sys import exit
+from random import random
 
-def gold_room():
-    print("This room is full of gold.  How much do you take?")
-    
-    choice = input("> ")
-    try:
-        how_much = int(choice)
-    except ValueError:
-        dead("Man, learn to type a number")
-    
-    if how_much < 50:
-        print("Nice, you are not greedy, you win!")
-        exit(0)
-    else:
-        dead("You greedy bastard!")
-        
+teams = { "A" : [{"name" :"Jurian", "spike" : 80, "reception" : 70, "jump" : [60, 90], "float" : [80, 40]}, 
+                 {"name" :"Giel"  , "spike" : 80, "reception" : 70, "jump" : [60, 90], "float" : [80, 40]},
+                 {"name" :"Arnout", "spike" : 80, "reception" : 70, "jump" : [60, 90], "float" : [80, 40]},
+                 {"name" :"Christ", "spike" : 80, "reception" : 70, "jump" : [60, 90], "float" : [80, 40]},
+                 {"name" :"Martin", "spike" : 80, "reception" : 70, "jump" : [60, 90], "float" : [80, 40]}],
 
-def bear_room():
-    print("There is a bear here.")
-    print("The bear has a bunch of honey.")
-    print("The fat bear is in front of another door.")
-    print("How are you going to move the bear?")
-    bear_moved = False
-    
-    while True:
-        choice = input("> ")
-        
-        if choice == "take honey":
-            dead("The bear looks at you then slaps your face off.")
-        elif choice == "taunt bear" and not bear_moved:
-            print("The bear had moved from the door.")
-            print("You can go through it now.")
-            bear_moved = True
-        elif choice == "taunt bear" and bear_moved:
-            dead("The bear gets pissed off and chews your leg off.")
-        elif choice == "open door" and bear_moved:
-            gold_room()
-        else:
-            print("I got no idea what that means.")
+         "B" : [ {"name" :"Sander", "secondBallFail" : 20, "reception" : 70, "jump" : [60, 90], "float" : [80, 40] }, 
+                 {"name" :"Casper", "spike" : 80, "reception" : 70, "jump" : [60, 90], "float" : [80, 40]}, 
+                 {"name" :"Jasper", "spike" : 80, "reception" : 70, "jump" : [60, 90], "float" : [80, 40]},
+                 {"name" :"Groen" , "spike" : 80, "reception" : 70, "jump" : [60, 90], "float" : [80, 40]},
+                 {"name" :"Tom"   , "spike" : 80, "reception" : 70, "jump" : [60, 90], "float" : [80, 40]},
+                 {"name" :"Ferry" , "spike" : 80, "reception" : 70, "jump" : [60, 90], "float" : [80, 40]}]}
 
+score = { "A" : 0, "B" : 0 }
+order = { "A" : 0, "B" : 0 }
 
 def serve(team):
-    print("Team",team,"is about to serve.")
-    print("It is player",teams[team][order])
-    print("Do you flee for your live, or eat your head?")
-    
-    choice = input("> ")
-    
-    if "flee" in choice:
-        start()
-    elif "head" in choice:
-        dead("Well, that was tasty!")
+    if team == 'A':
+        receptor = 'B'
     else:
-        cthulhu_room()
+        receptor = 'A'
+        
+    print("Team",team,"is about to serve.")
+    print("It is player",teams[team][order[team]%6]["name"])
+    
+    if team == "A":
+        print("Do you go for a jump or a float?")
+        choice = input("> ")
+    else:
+        if random() > 0.24:
+            choice = "float"
+        else:
+            choice = "jump"
+            
+    if "jump" in choice.lower():
+        choice = 'jump'
+    elif "float" in choice.lower():
+        choice = 'float'
+    else:
+        print("You failed to choose a valid serve, point lost.")
+        order[receptor]+=1
+        point(receptor)
+        
+    if teams[team][order[team]%6][choice][0]/100.0 < random():
+        print("The serve was not valid, point lost.")
+        order[receptor]+=1
+        point(receptor)        
+    else:
+        reception(receptor,teams[team][order[team]%6][choice][1]*random())
 
+def reception(team,strength):
+    avgReception = 0
+    if team == 'A':
+        server = 'B'
+    else:
+        server = 'A'
+        
+    for player in teams[team]:
+        avgReception += player["reception"]/6.0
+    
+    if avgReception > strength:
+        print('The reception was succesful:',avgReception,strength,'\nPoint scored')
+        order[team]+=1
+        point(team)
+    else:
+        print('The serve was to strong:',avgReception,strength,'\nAce scored')
+        point(server)
+        
+def point(team):
+    score[team]+=1
+    print(f'''Score:
+    team A    :   team B
+ {score["A"]} : {score["B"]}
+''')
+    if score["A"] >= 25 or score["B"] >= 25:
+        if abs(score["A"]-score["B"]) >= 2:
+            print('set afgelopen.')
+            finished(score)
+        else:
+            print('2 punten verschil nodig')
+            serve(team)
+    else:
+        serve(team)
 
 def finished(score):
-    if score[0] > score [1]:
-        print(why, "You won, well done!")
+    if score["A"] > score ["B"]:
+        print("You won, well done!")
     else:
-        print(why, "You lost, too bad!")
+        print("You lost, too bad!")
         
     exit(0)
 
 def start():
     print("You are the setter in a volleyball game.")
-    print("I am the referee, what ia your name?")
+    print("I am the referee, what is your name?")
     
-    choice = input("> ")
+    name = input("> ")
     
-    if 'jeroen" in choice.lower():
+    if 'jeroen' in name.lower():
         secondBallFail = 100
+        floatServe = [80,40]
+        jumpserve = [20,80]
     else:
         secondBallFail = 50
-
+        floatServe = [70, 50]
+        jumpserve = [40, 90]
     
+    teams['A'].insert(0, {"name" : name.capitalize(), "secondBallFail" : secondBallFail, "reception" : 70, "jump" : jumpserve, "float" : floatServe })
+    print("The toss is next, will you choose heads or tails?")
+    toss_choice = input("> ")
+    
+    if random() > 0.5:
+        print(f"It's {toss_choice}! Do you want to serve or receive the first serve?")
+        first_serve = input("> ")
+        if "serve" in first_serve:
+            serve("A")
+        else:
+            serve("B")
+    else:
+        if "heads" in toss_choice:
+            result = "tails"
+        else:
+            result = "heads"
+        print(f"It's {result}! The opponent chooses to serve.")
+        serve("B")
+        
+#oldserve = ''    
 start()
